d1e2c8d337908d4d50a09d86fcee3666aa74222b
MdBilal420 <mohdbilal16@gmail.com>
2020-07-04 11:23:32 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Loading from './Loading';
 
-function App() {
-  return (
-    <div className="App">
-     <h1> hell world </h1>
-    </div>
-  );
+class App extends Component{
+    constructor(props){
+
+    super(props)
+        //state
+    this.state = {
+        users: [],
+        loading : false
+    }
+
+    //bind
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+getUsers(){
+
+    this.setState({
+        loading : true
+    })
+
+    axios('https://randomuser.me/api/?nat=US&results=5').then(response=>this.setState({
+    users : [...this.state.users, ...response.data.results],
+    loading : false
+        })
+    );
+}
+
+handleSubmit(e){
+    e.preventDefault();
+    this.getUsers();
+    console.log("more users");
+}
+
+componentDidMount(){
+    this.getUsers();
+}
+
+render(){
+    const { loading, users } = this.state;
+    return (<div className="App">
+        <form onSubmit={this.handleSubmit}>
+                <input type="submit" value="load users"/>
+        </form>
+        <hr />
+     {!loading ? 
+        users.map(user => 
+        <div key={user.id.value}>
+             <h3>{user.name.first}</h3>
+            <p>{user.email}</p>
+            <hr />
+        </div>
+        ) : <Loading message="hiiii" />}
+        </div>
+    );
+    }
 }
 
 export default App;
